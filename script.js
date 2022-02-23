4b592faab72df3945b79f8cc03bff3a34bba0b1a
TermFerd <tford2035@gmail.com>
2022-02-23 22:58:31 +0000

    we gettin' there!

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ function createGrid (gridNumber) {
     let gridArea = gridNumber * gridNumber;
     for (let i = 1; i <= gridArea; i++) {
         let gridItem = document.createElement('div');
+        gridItem.className = 'gridSquare';
         container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
         container.insertAdjacentElement('beforeend', gridItem);
     } 
 }
 
+function squareColorChange () {
 
-createGrid(10);
+
+}
+
+
+
+
+createGrid(16);
+
 
 
 
