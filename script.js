cdc53f1e6ff22560feb78a5f16f842c745922c7a
TermFerd <tford2035@gmail.com>
2022-02-23 22:38:21 +0000

    got the grid working!
    now need to change the grid color as you hover

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+
+function createGrid (gridNumber) { 
+    let gridArea = gridNumber * gridNumber;
+    for (let i = 1; i <= gridArea; i++) {
+        let gridItem = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+        container.insertAdjacentElement('beforeend', gridItem);
+    } 
+}
+
+
+createGrid(10);
+
+
+
+
+
+
